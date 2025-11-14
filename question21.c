a6bfcaa741b20f7d7be83b84edac12e05a2dfce8
gemasandinom <gemasandinom@gmail.com>
2025-11-14 12:22:12 -0600

    Completed program for question 21

---
+++ question21.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+	int rows, i, j;
+
+	printf("Enter number of rows: ");
+	scanf("%d", &rows);
+
+	for (i = 0; i < rows; i++) {
+		for (j = rows - i - 1; j >= 1; j--) {
+			printf(" ");
+		}
+		for (j = 0; j <= i; j++) {
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
