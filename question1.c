f6de4b01319361ad9ba91057f0ef40bf985d081c
gemasandinom <gemasandinom@gmail.com>
2025-11-14 15:13:41 -0600

    Added comments to program 1

--- question1.c
+++ question1.c
@@ -1,7 +1,14 @@
+/*
+ * Documentation Section
+ * Program: Area and Perimeter of a rectangle
+*/
+
+/* Link Section */
 #include <stdio.h>
 
+//Main Function Section
 int main(void) {
-	double length, width, area, perimeter;
+	double length, width, area, perimeter; //Declaration part
 
 	printf("Enter the length of the rectangle: ");
 	scanf("%lf", &length);
@@ -9,11 +16,12 @@ int main(void) {
 	printf("Enter the width of the rectangle: ");
 	scanf("%lf", &width);
 
+	//Executable part
 	area = length * width;
 	perimeter = 2 * (length + width);
 
 	printf("Area: %.2lf\n", area);
 	printf("Perimeter: %.2lf\n", perimeter);
 
-	return 0;
+	return 0; //End of main
 }
